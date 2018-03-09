6ed96a4235ded5c4055773eadf22379f56197dce
ksaper <kimo.s@qualisystems.com>
2018-03-09 10:16:14 -0800

    Added timeout for multiple outlet calls
    Found a bug where if more than 1 SNMP Set was sent, it would only
    process the first and ignore the rest - added a timeout to stagger the
    calls

--- src/sentry/pm_pdu_handler.py
+++ src/sentry/pm_pdu_handler.py
@@ -5,6 +5,8 @@ from pysnmp.proto.rfc1902 import Integer
 from pysnmp.smi.rfc1902 import ObjectIdentity
 from time import sleep
 
+TIMEOUT = 1  # to stagger multiple port calls (SNMP can drop if received to quickly)
+
 
 class PmPduHandler:
     class Port:
@@ -37,7 +39,7 @@ class PmPduHandler:
             self.logger.info("Powering on port %s" % raw_port)
             self.snmp_handler.set(ObjectIdentity('Sentry4-MIB', 'st4OutletControlAction', port.port_number, port.pdu_number, port.outlet_number),
                                   Integer(1))
-
+            sleep(TIMEOUT)
 
     def power_off(self, port_list):
         self.logger.info("Power off called for ports %s" % port_list)
@@ -46,6 +48,7 @@ class PmPduHandler:
             port = self.Port(raw_port)
             self.snmp_handler.set(ObjectIdentity('Sentry4-MIB', 'st4OutletControlAction', port.port_number, port.pdu_number, port.outlet_number),
                                   Integer(2))
+            sleep(TIMEOUT)
 
     def power_on(self, port_list):
         self.logger.info("Power on called for ports %s" % port_list)
@@ -54,3 +57,4 @@ class PmPduHandler:
             port = self.Port(raw_port)
             self.snmp_handler.set(ObjectIdentity('Sentry4-MIB', 'st4OutletControlAction', port.port_number, port.pdu_number, port.outlet_number),
                                   Integer(1))
+            sleep(TIMEOUT)
