4ca3f663329cbaad9424fa20d2bb1b68f404612a
Tim Spiglanin <timothy.spiglanin@gmail.com>
2018-12-06 15:13:20 -0800

    Clean-up Debuggng
    Cleaning up stored debugging values

--- src/driver.py
+++ src/driver.py
@@ -148,8 +148,7 @@ if __name__ == "__main__":
     context.reservation = mock.MagicMock()
     context.connectivity = mock.MagicMock()
     context.reservation.reservation_id = "<Reservation ID>"
-#    context.resource.address = "10.11.100.251"  # Sentry 4
-    context.resource.address = "10.16.145.249"  # Sentry 3
+    context.resource.address = "<IP>"  # Sentry 4
     context.resource.name = "Debug_Sentry4"
     context.resource.attributes = dict()
     context.resource.attributes["{}.User".format(shell_name)] = "admn"
